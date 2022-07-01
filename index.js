b2918100ceb86bc96961ab3fd2d7792e78edb27f
João Marcos <80593996+Joao-Sant-Ana@users.noreply.github.com>
2022-07-01 11:36:38 -0300

    V0.1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-document.writeln('Sistema de identificação de alunos')
+document.writeln('<h1>Sistema de identificação de alunos</h1>')
 var nome = window.prompt('Digite seu nome: ')
 var ano = window.prompt('Qual foi o ano em que voê nasceu?')
 var anoAtual = new Date().getFullYear()
 let dias = Number((anoAtual - ano) * 365)
 document.writeln(`Você ja viveu ${dias} dias.`)
+document.writeln(`<h2>Bem vindo ao sistema ${nome}!</h2>`)
